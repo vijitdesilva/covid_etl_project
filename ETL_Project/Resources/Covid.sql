d045a1240a996ef436a309d5fcfd5ba2a4dcde99
vijitdesilva <vijit.desilva@gmail.com>
2021-06-20 23:00:48 -0400

    final

---
+++ ETL_Project/Resources/Covid.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "PCR_TEST_POSITIVE"(
+state VARCHAR NOT NULL,
+date DATE NOT NULL,
+new_results_reported INT NOT NULL,
+total_results_reported INT NOT NULL,
+geocoded_state VARCHAR NOT NUll	
+)
+
+SELECT* FROM PCR_TEST_POSITIVE;
